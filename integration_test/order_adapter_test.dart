fe6385f1e598e2d84df04326c6245b0e02c75ef6
Curtis Schwoebel <curtis.schwoebel@gmail.com>
2022-10-13 23:59:07 +0200

    update gradle settings for google authentication, add config to test out firestore faker

--- integration_test/order_adapter_test.dart
+++ integration_test/order_adapter_test.dart
@@ -7,8 +7,18 @@ import 'package:egg_app/domain/model/user.dart';
 import 'package:flutter_simple_dependency_injection/injector.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
+import 'package:fake_cloud_firestore/fake_cloud_firestore.dart';
+
 
 Future<void> main() async {
+  final instance = FakeFirebaseFirestore();
+  await instance.collection('users').add({
+    'username': 'Bob',
+  });
+  final snapshot = await instance.collection('users').get();
+  print(snapshot.docs.length); // 1
+  print(snapshot.docs.first.get('username')); // 'Bob'
+  print(instance.dump());
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
   final injector = await ModuleContainer().initialise(Injector());
   OrderAdapter orderAdapter = injector.get<OrderAdapter>();
